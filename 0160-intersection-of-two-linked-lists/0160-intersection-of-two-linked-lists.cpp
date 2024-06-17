66000a7feb2090ab7fbd877ebda1f9c86c01ccb4
Anastasia Radchenko <42119559+Aidan2803@users.noreply.github.com>
2024-06-17 03:28:44 +0300

    Time: 61 ms (12.24%), Space: 17.1 MB (35.07%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -6,28 +6,78 @@
  *    ListNode(int x) : val(x), next(NULL) {}
  *};
  */
-class Solution
-{
-    public:
-        ListNode* getIntersectionNode(ListNode *headA, ListNode *headB)
-        {
-            std::unordered_map<ListNode*, ListNode*> hash_table;
+class Solution {
+public:
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        // Solution when we have additional space
+        //     std::unordered_map<ListNode*, ListNode*> hash_table;
 
-            ListNode* currentA = headA;
-            while(currentA != nullptr){
-                hash_table.insert(std::pair<ListNode*, ListNode*>(currentA, currentA));
-                currentA = currentA->next;
-            }
+        //     ListNode* currentA = headA;
+        //     while(currentA != nullptr){
+        //         hash_table.insert(std::pair<ListNode*, ListNode*>(currentA,
+        //         currentA)); currentA = currentA->next;
+        //     }
 
-            ListNode* currentB = headB;
-            while(currentB != nullptr){
-                if(hash_table.count(currentB) > 0){
-                    return currentB;
-                }
-                hash_table.insert(std::pair<ListNode*, ListNode*>(currentB, currentB));
-                currentB = currentB->next;
-            }
+        //     ListNode* currentB = headB;
+        //     while(currentB != nullptr){
+        //         if(hash_table.count(currentB) > 0){
+        //             return currentB;
+        //         }
+        //         hash_table.insert(std::pair<ListNode*, ListNode*>(currentB,
+        //         currentB)); currentB = currentB->next;
+        //     }
 
+        //     return nullptr;
+        // }
+
+        // Solution without additional space
+
+        if(headA == nullptr || headB == nullptr){
             return nullptr;
         }
+
+        ListNode* currentA = headA;
+        ListNode* currentB = headB;
+
+        int sizeA = 0;
+        while (currentA != nullptr) {
+            sizeA++;
+            currentA = currentA->next;
+        }
+
+        int sizeB = 0;
+        while (currentB != nullptr) {
+            sizeB++;
+            currentB = currentB->next;
+        }
+
+        std::cout << "size A " << sizeA << "\n";
+        std::cout << "size B " << sizeB << "\n";
+
+        auto diff = std::abs(sizeA - sizeB);
+
+        currentA = headA;
+        currentB = headB;
+        if (sizeA < sizeB) {
+            for (int i = 0; i < diff; i++) {
+                currentB = currentB->next;
+            }
+        } else {
+            for (int i = 0; i < diff; i++) {
+                std::cout << "diff " << diff << "\n";
+                currentA = currentA->next;
+            }
+        }
+
+        while (currentA != nullptr && currentB != nullptr) {
+            std::cout << "currentA " << currentA << " currentB " << currentB << "\n";
+            if (currentA == currentB) {
+                return currentA;
+            }
+            currentA = currentA->next;
+            currentB = currentB->next;
+        }
+
+        return nullptr;
+    }
 };
