acf50adc44265beee106c473fa47eb8ba680e5a3
Anastasia Radchenko <42119559+Aidan2803@users.noreply.github.com>
2025-08-12 01:37:37 +0300

    Time: 0 ms (100%), Space: 9.8 MB (26.24%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -6,24 +6,13 @@ public:
       return false;
     }
 
-    std::unordered_map<char, int> map_s;
+    std::unordered_map<char, int> map;
+
     for (auto c : s) {
-      map_s[c]++;
+      map[c]++;
     }
-
-    std::unordered_map<char, int> map_t;
     for (auto c : t) {
-      map_t[c]++;
-    }
-
-    for (int i = 0; i < s.size(); i++) {
-      std::cout << "map_s[i] " << map_s[s[i]] << " map_t[i] " << map_t[s[i]]
-                << " s: " << s[i] << "\n";
-      if (map_s.find(s[i]) != map_s.end() && map_t.find(s[i]) != map_t.end()) {
-        if (map_s[s[i]] != map_t[s[i]]) {
-          return false;
-        }
-      } else {
+      if (--map[c] < 0) {
         return false;
       }
     }
