c9e7cdc9e801be35e0c6cb3c413d784a713e58ca
Anastasia Radchenko <42119559+Aidan2803@users.noreply.github.com>
2025-08-12 03:05:47 +0300

    Time: 45 ms (72.82%), Space: 91 MB (32.31%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,7 +3,6 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         std::unordered_map<int,int> map;
         for(int number : nums){
-            std::cout << map[number] << " " << number << "\n";
             if(++map[number] > 1){
                 return true;
             }
