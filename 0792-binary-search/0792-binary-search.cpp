97a422e2b225293353e947d99d468e4b86be6dce
Anastasia Radchenko <42119559+Aidan2803@users.noreply.github.com>
2025-08-15 01:46:10 +0300

    Time: 0 ms (100%), Space: 31.5 MB (9.59%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-int search(vector<int>& nums, int target) {
-    int i = 0;
-    int j = nums.size() - 1;
-    
-    while (i <= j) {
-        int k = i + (j - i) / 2; // avoids overflow
+    int search(vector<int>& nums, int target) {
+        int i = 0;
+        int j = nums.size() - 1;
         
-        if (nums[k] > target) {
-            j = k - 1;
+        while(i <= j){
+            int k = i + (j - i) / 2;
+            std::cout << "k: " << k << "\n";
+            if(nums[k] > target){
+                j = k - 1;
+                std::cout << "nums[k] > target\n";
+            }
+            else if (nums[k] < target){
+                i = k + 1;
+                std::cout << "nums[k] < target\n";
+            }
+            else if (nums[k] == target){
+                return k;
+            }
         }
-        else if (nums[k] < target) {
-            i = k + 1;
-        }
-        else {
-            return k;
-        }
+        return -1;
     }
-    return -1;
-}
 };
