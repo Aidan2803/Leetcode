e1f922fd9e4e84050d84266c8879367b9d581f51
Anastasia Radchenko <42119559+Aidan2803@users.noreply.github.com>
2025-08-21 01:10:20 +0300

    Time: 0 ms (100%), Space: 19.6 MB (29.45%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -13,57 +13,28 @@ class Solution
     public:
         ListNode* mergeTwoLists(ListNode *list1, ListNode *list2)
         {
-            if (list1 == nullptr)
-            {
-                return list2;
-            }
-            if (list2 == nullptr)
-            {
-                return list1;
-            }
+            ListNode dummy;
+            ListNode *tail = &dummy;
 
-            ListNode *newlist = nullptr;
-            ListNode *newlist_head = nullptr;
+        while(list1 && list2){
+           if(list1->val < list2->val){
+            tail->next = list1;
+            list1 = list1->next;
+           } else{
+            tail->next = list2;
+            list2 = list2->next;
+           }
+           tail = tail->next;
+        }
+        
+        if(!list1){
+            tail->next = list2;
+        }
 
-            if (list1->val <= list2->val)
-            {
-                newlist = list1;
-                list1 = list1->next;
-            }
-            else
-            {
-                newlist = list2;
-                list2 = list2->next;
-            }
-
-            newlist_head = newlist;
-
-            while (list1 != nullptr && list2 != nullptr)
-            {
-                if (list1->val <= list2->val)
-                {
-                    newlist->next = list1;
-                    list1 = list1->next;
-                }
-                else
-                {
-                    newlist->next = list2;
-                    list2 = list2->next;
-                }
-                newlist = newlist->next;
-            }
-
-            if (list1 != nullptr)
-            {
-                newlist->next = list1;
-            }
-            else
-            {
-                newlist->next = list2;
-            }
-
-            ListNode *node = newlist_head;
-
-            return newlist_head;
+        if(!list2){
+            tail->next = list1;
+        }
+ 
+        return dummy.next;
         }
 };
