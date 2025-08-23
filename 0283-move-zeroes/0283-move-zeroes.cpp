167f48fadde89417aaa1776c7ab02a0ed500d18f
Anastasia Radchenko <42119559+Aidan2803@users.noreply.github.com>
2025-08-23 17:30:43 +0300

    Time: 0 ms (100%), Space: 23.7 MB (81.77%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
   void moveZeroes(std::vector<int> &nums) {
-    int zeroes_amount = 0;
-    for(int i = nums.size() - 1; i >= 0; i--){
-        if(nums[i] == 0){
-            zeroes_amount++;
-            nums.erase(nums.begin() + i);
+    int lastNonZero = 0;
+
+    for(int i = 0; i < nums.size(); i++){
+        if(nums[i] != 0){
+            nums[lastNonZero] = nums[i];
+            lastNonZero++;
         }
     }
 
-    for(int i = 0; i < zeroes_amount; i++){
-        nums.push_back(0);
+    for(int i = lastNonZero; i < nums.size(); i++){
+        nums[i] = 0;
     }
+
   }
 };
