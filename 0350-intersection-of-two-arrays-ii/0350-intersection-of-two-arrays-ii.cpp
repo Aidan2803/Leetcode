324b2ffd5fb22f03d61929f10932a6e6ab9439a0
Anastasia Radchenko <42119559+Aidan2803@users.noreply.github.com>
2025-08-25 01:33:34 +0300

    Time: 0 ms (100%), Space: 14.2 MB (75.8%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,25 +1,54 @@
 class Solution {
 public:
+    // Initial variant of solution
+    // vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+    //     if(nums1.size() > nums2.size()) {
+    //         return intersect(nums2, nums1); // ensure nums1 is smaller
+    //     }
+
+    //     std::unordered_map<int, int> number_to_amount;
+    //     std::vector<int> result;
+
+    //     for(int num : nums1){
+    //         number_to_amount[num]++;
+    //     }
+
+    //     for(int num : nums2){
+    //         auto it = number_to_amount.find(num);
+    //         if(it != number_to_amount.end() && it->second > 0){
+    //             result.push_back(num);
+    //             it->second--;
+    //         }
+    //     }
+
+    //     return result;
+    // }
+
+    // If arrays are already sorted
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size() > nums2.size()) {
-            return intersect(nums2, nums1); // ensure nums1 is smaller
-        }
 
-        std::unordered_map<int, int> number_to_amount;
-        std::vector<int> result;
+        std::vector<int> result_vector;
+        // sort for testing
+        std::sort(nums1.begin(), nums1.end());
+        std::sort(nums2.begin(), nums2.end());
 
-        for(int num : nums1){
-            number_to_amount[num]++;
-        }
+        int first_arr_index = 0;
+        int second_arr_index = 0;
 
-        for(int num : nums2){
-            auto it = number_to_amount.find(num);
-            if(it != number_to_amount.end() && it->second > 0){
-                result.push_back(num);
-                it->second--;
+        while(first_arr_index < nums1.size() && second_arr_index < nums2.size()){
+            if (nums1[first_arr_index] < nums2[second_arr_index]){            first_arr_index++;}
+            else if(nums1[first_arr_index] > nums2[second_arr_index]){
+                second_arr_index++;
             }
-        }
+            else {
+                result_vector.push_back(nums1[first_arr_index]);
+                first_arr_index++;
+                second_arr_index++;
+            }
 
-        return result;
+
+        }
+        return result_vector;
     }
+
 };
